215d9ae7d0b1eead2d6022bc97a1503b5c8233f6
NipunSGeeTH <sangeethnipun385@gmail.com>
2026-03-31 10:21:05 +0530

    init: key tone generator

---
+++ 8-key-tone-generator/8-key-tone-generator.ino
@@ -0,0 +1,66 @@
+/*
+Name: K.M.N.Sangeeth Kariyapperuma
+Index Number: 230317J
+Task: 8-Key Tone Generator
+*/
+
+const int buzzerPin = 8;
+
+
+const int buttonPins[8] = {2, 3, 4, 5, 6, 7, 9, 10};
+
+const int frequencies[8] = {300, 400, 500, 600, 700, 800, 900, 1000};
+
+
+bool prevState[8];
+
+unsigned long pressTime = 0;
+bool shortToneActive = false;
+
+void setup() {
+  for (int i = 0; i < 8; i++) {
+    pinMode(buttonPins[i], INPUT_PULLUP);
+    prevState[i] = HIGH;
+  }
+}
+
+void loop() {
+  int totalFrequency = 0;
+  bool anyPressed = false;
+
+
+  for (int i = 0; i < 8; i++) {
+    bool currState = digitalRead(buttonPins[i]);
+
+
+    if (prevState[i] == HIGH && currState == LOW) {
+      pressTime = millis();
+      shortToneActive = true;
+    }
+
+    if (currState == LOW) {
+      totalFrequency += frequencies[i];
+      anyPressed = true;
+    }
+
+    prevState[i] = currState;
+  }
+
+  if (shortToneActive) {
+    if (millis() - pressTime <= 20) {
+      if (anyPressed) {
+        tone(buzzerPin, totalFrequency);
+      }
+    } else {
+      shortToneActive = false;
+    }
+  } 
+  else {
+
+    if (anyPressed) {
+      tone(buzzerPin, totalFrequency);
+    } else {
+      noTone(buzzerPin);
+    }
+  }
+}
---
+++ 8-key-tone-generator/Readme.md
@@ -0,0 +1,10 @@
+# Task #1:
+
+
+The tone generator will have 8 push-button switches read by the Arduino uno-board as digital inputs.  Pressing each of the keys should generate a tone through a speaker connected to the Arduino board via a digital output.  The eight keys should be assigned with the following tone-frequencies; 300Hz, 400Hz, 500Hz, 600Hz, 700Hz, 800Hz, 900Hz and 1kHz.  Pressing the key momentarily should generate the tone for 20mS while pressing and holding the key should generate the tone continuously without any break.  When more than one key is pressed at the same time, the generated tone should have a frequency equivalent to the sum of the frequencies assigned to each key.
+
+Design your circuit using Tinker Cad (preferred) or Wokwi simulator.  Once the Task is completed submit the following under the specific links provided.
+
+1.      A full screen image of the circuit, including the date and time displayed in the menu bar (e.g. at the bottom right for a Windows computer)
+
+2.      Arduino code developed for exercise.  Include your name and the index number as comments in the top of the Arduino script.
